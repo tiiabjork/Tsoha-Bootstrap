2cb79b7bb9dccf289383c073d5e48dc8a6bcff91
Tiia Björk <tiia.bjork@gmail.com>
2017-12-28 19:21:42 +0200

    Taulujen pystyttäminen ja testidatan sisäänvieminen

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,13 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (kayttajatunnus, salasana)
+VALUES ('Ville', 'omena123');
+
+INSERT INTO Kayttaja (kayttajatunnus, salasana) 
+VALUES ('Mia', 'koira');
+
+INSERT INTO Askare (laatija, kuvaus, kiireellisyys)
+VALUES (NULL, 'Tiskivuoren tuhoaminen', 5);
+
+INSERT INTO Luokka (laatija, luokkakuvaus)
+VALUES (NULL, 'Kotityö');
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,24 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  kayttaja_id SERIAL PRIMARY KEY, 
+  kayttajatunnus varchar(50) NOT NULL, 
+  salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Askare(
+  atunnus SERIAL PRIMARY KEY,
+  laatija INTEGER REFERENCES Kayttaja(kayttaja_id),
+  kuvaus varchar(120) NOT NULL,
+  kiireellisyys INTEGER,
+  onkoLuokkia boolean DEFAULT FALSE
+);
+
+CREATE TABLE Luokka(
+  ltunnus SERIAL PRIMARY KEY,
+  laatija INTEGER REFERENCES Kayttaja(kayttaja_id), 
+  luokkakuvaus varchar(20)
+);
+
+CREATE TABLE Askareen_luokka(
+  atunnus INTEGER REFERENCES Askare(atunnus), 
+  ltunnus INTEGER REFERENCES Luokka(ltunnus)
+);
